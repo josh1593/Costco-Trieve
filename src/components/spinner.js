5d4ab3feac347fb816b6aac657233758d28c8d69
Eyasu Nigussie <eyasunigussie@Eyasus-MacBook-Air.local>
2024-06-26 19:57:48 -0700

    fix

--- src/components/spinner.js
+++ src/components/spinner.js
@@ -3,7 +3,7 @@ import { ThreeDots } from 'react-loader-spinner';
 
 const Spinner = () => {
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', marginLeft: '36rem' }}>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginLeft: '36rem', height: '60vh'}}>
       <ThreeDots color="#00BFFF" height={80} width={80} />
     </div>
   );
